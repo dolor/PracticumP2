79bd55876bcfcf124bdc548014b2eed8937f2364
Erik Steenman <erik.dolor@gmail.com>
2012-03-12 21:09:19 +0100

    Added eindopdracht package and one file to make the commit

---
+++ eindopdracht/bord/BordPanel.java
@@ -0,0 +1,12 @@
+package eindopdracht.bord;
+
+import javax.swing.JPanel;
+
+public class BordPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2232170303403827378L;
+
+}
