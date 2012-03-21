80c881bb2942d091d092bf0c4a074cfed061eab2
Erik Steenman <erik.dolor@gmail.com>
2012-03-21 16:57:45 +0100

    Made a MainWindow that can connect to a server. NewGame window still needs to be implemented though.

--- eindopdracht/client/gui/Connect.java
+++ eindopdracht/client/gui/Connect.java
@@ -7,18 +7,22 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import eindopdracht.util.NetworkUtil;
 
-public class Connect extends JFrame implements ActionListener {
+public class Connect extends JFrame implements ActionListener, DocumentListener {
 
 	private JTextField hostnameTextField;
 	private JTextField portTextField;
 	private JButton cancelButton;
 	private JButton okayButton;
+	private MainWindow observer;
 
-	public Connect() {
+	public Connect(MainWindow observer) {
 		super("Connect");
+		this.observer = observer;
 		buildGUI();
 		setVisible(true);
 
@@ -40,10 +44,14 @@ public class Connect extends JFrame implements ActionListener {
 		
 		this.add(new JLabel("Host"));
 		hostnameTextField = new JTextField(15);
+		hostnameTextField.getDocument().addDocumentListener(this);
+		hostnameTextField.addActionListener(this);
 		this.add(hostnameTextField);
 		
 		this.add(new JLabel("Port"));
 		portTextField = new JTextField(15);
+		portTextField.getDocument().addDocumentListener(this);
+		portTextField.addActionListener(this);
 		this.add(portTextField);
 		
 		cancelButton = new JButton("Cancel");
@@ -51,11 +59,13 @@ public class Connect extends JFrame implements ActionListener {
 		this.add(cancelButton);
 		okayButton = new JButton("OK");
 		okayButton.addActionListener(this);
+		okayButton.setEnabled(false);
 		this.add(okayButton);
 	}
 	
 	public void sendForm() {
-		System.out.println("Connecting to " + hostnameTextField.getText() + "(" + portTextField.getText() + ")");
+		observer.connect(hostnameTextField.getText(), Integer.parseInt(portTextField.getText()));
+		observer.connectFrameDismissed();
 		this.dispose();
 	}
 
@@ -74,6 +84,34 @@ public class Connect extends JFrame implements ActionListener {
 					sendForm();
 			}
 			System.out.println("Connecting to " + hostnameTextField.getText());
+		} else if (ev.getSource().equals(hostnameTextField) || ev.getSource().equals(portTextField)) {
+			if (okayButton.isEnabled())
+				okayButton.doClick();
+		}
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent sender) {
+		//if (sender.getDocument().equals(hostnameTextField.getDocument())) {
+			
+		//}
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent sender) {
+		if (NetworkUtil.isValidHost(hostnameTextField.getText()) && NetworkUtil.isValidPort(portTextField.getText())) {
+			okayButton.setEnabled(true);
+		} else {
+			okayButton.setEnabled(false);
+		}
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent sender) {
+		if (NetworkUtil.isValidHost(hostnameTextField.getText()) && NetworkUtil.isValidPort(portTextField.getText())) {
+			okayButton.setEnabled(true);
+		} else {
+			okayButton.setEnabled(false);
 		}
 	}
 }
