e3505ed98b0eace02a003f5f00c6addb4288f0cc
unknown <mickvdv@mickvdv-HP.(none)>
2012-03-30 15:51:26 +0200

    Bugfixed

--- eindopdracht/Client.java
+++ eindopdracht/Client.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import eindopdracht.client.gui.PentagoXLWindow;
 
 public class Client {
+	// TODO: Balletjes, werkend maken, endgame en disconnect
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
