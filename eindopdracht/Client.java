4ab297b6e423acc602b0fe88b9a99b617a4750df
unknown <mickvdv@mickvdv-HP.(none)>
2012-04-03 15:15:43 +0200

    TODO

--- eindopdracht/Client.java
+++ eindopdracht/Client.java
@@ -10,6 +10,7 @@ import eindopdracht.client.gui.PentagoXLWindow;
 	// TODO: Op een quit / end_game van de server moet de client er niet uitklappen
 	// TODO: AI crasht als de server niet bestaat.
 	// TODO: Geen lege chat versturen
+	// TODO: Exit knop werkt niet
 public class Client {
 	public static void main(String[] args) {
 		new MainController();
