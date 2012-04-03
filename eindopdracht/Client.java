4ef2923e2929a5f2620c76e5db04ca3337c45b87
Erik Steenman <erik.dolor@gmail.com>
2012-04-03 15:15:22 +0200

    Moar bug fixing

--- eindopdracht/Client.java
+++ eindopdracht/Client.java
@@ -5,11 +5,8 @@ import java.awt.EventQueue;
 import eindopdracht.client.MainController;
 import eindopdracht.client.gui.PentagoXLWindow;
 
-	// TODO: Speler met dezelfde naam? Gaat dat goed
-	// TODO: Balletjes, werkend maken, endgame en disconnect
 	// TODO: Op een quit / end_game van de server moet de client er niet uitklappen
 	// TODO: AI crasht als de server niet bestaat.
-	// TODO: Geen lege chat versturen
 public class Client {
 	public static void main(String[] args) {
 		new MainController();
